e6e4b3408627ed2fe7928415626ff42b828c8ff7
Daniel Duran <vendraminidaniel@gmail.com>
2019-04-22 16:58:04 -0400

    Tokens Adicionados (fim da aula #7)

--- Routes/index.js
+++ Routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router();
+const auth = require('../middlewares/auth.js')
 
-router.get('/', (req, res) => {
-    return res.send({message: "Tudo ok com o método GET da raiz"})
+router.get('/', auth, (req, res) => {
+    console.log(res.locals.auth_data);
+    return res.send({message: "Essa informação é muito importante apenas usuários autorizados podem têla"})
 })
 
 router.post('/', (req, res) => {
@@ -10,3 +12,10 @@ router.post('/', (req, res) => {
 })
 
 module.exports = router;
+
+
+//user 3 = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjYmUxZWEwNWViY2JkMTgyYzc4YWNmNyIsImlhdCI6MTU1NTk2MzU1MiwiZXhwIjoxNTU2NTY4MzUyfQ.GSP_JZ8-T9m9QmWB9mLC8dIrbguI4CZVYIEO6gSNF6Q
+
+//user 4 = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjYmUyMTRjZjVlMzgwMzUzNDFkYjQ0NCIsImlhdCI6MTU1NTk2NDIzNiwiZXhwIjoxNTU2NTY5MDM2fQ.-D1DIpYI8_SfbGkRZkMuxjx9vICsOrtO0OqU6oUHxeM
+
+//user 5 = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjYmUyMjgyZjUwOGViMzcyY2ZkOGIzMiIsImlhdCI6MTU1NTk2NDU0NiwiZXhwIjoxNTU2NTY5MzQ2fQ.f9IYIpSpwj5dn841346gVKBTznzzUTIEo0pG4m7Iz_E
--- Routes/users.js
+++ Routes/users.js
@@ -2,6 +2,12 @@ const express = require('express')
 const router = express.Router();
 const Users = require('../model/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+//FUNÇÕES AUXILIARES (jwt)
+const createUserToken = (userId) => {
+    return jwt.sign({id: userId}, 'batatafrita2019', {expiresIn: '7d'})
+}
 
 router.get('/', async (req, res) => {
     try {
@@ -26,7 +32,7 @@ router.post('/create', async (req, res) => {
 
         const user = await Users.create(req.body)
         user.password = undefined
-        return res.send(user)
+        return res.send({user, token: createUserToken(user.id)})
     }
     catch (err) {
         return res.send({ error: "Erro ao buscar usuário"})
@@ -46,7 +52,7 @@ router.post('/auth', async (req, res) => {
         if(!pass_ok) return res.send({ error: 'Erro ao autenticar usuário'})
 
         user.password = undefined
-        return res.send(user)
+        return res.send({user, token: createUserToken(user.id)})
     }
     catch (err) {
         return res.send({erro: 'Erro ao buscar usuário'})
